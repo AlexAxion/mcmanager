26975f75fd283426dadf10751f416a1d2fc11e28
AlexAxion <alexaxion115@gmail.com>
2025-02-12 18:15:49 -0800

    Your commit message

--- mcmanager/src/main.rs
+++ mcmanager/src/main.rs
@@ -1,14 +1,15 @@
 use termion::event::Key;
 use termion::raw::IntoRawMode;
 use termion::input::TermRead;
-use termion::{clear, cursor};
+use termion::style::CrossedOut;
+use termion::{clear, cursor, color};
 use std::io::{stdin, stdout, Write};
 mod tools;
 
 fn main(){
     
     let mut current_location = (1, 1);
-    
+    main_menu(&mut current_location);
     let stdin = stdin();
     let mut _stdout = stdout().into_raw_mode().unwrap();
 
@@ -30,23 +31,47 @@ fn hover_location(current_location: &mut (i16, i16) , update_location: (i16, i16
 
 
     match  (current_location.0 + update_location.0, current_location.1 + update_location.1) {
-        (1..,1..) => {*current_location = (current_location.0 + update_location.0, current_location.1 + update_location.1);
+        (1..2,1..) => {*current_location = (current_location.0 + update_location.0, current_location.1 + update_location.1);
                     main_menu(current_location);
         },
         _ => return,
 
     };
      
-    //write!(stdout(), "{}{}\r({}, {})\r", clear::All, cursor::Goto(1,1), &current_location.0, &current_location.1).unwrap();
+    //write!(stdout(), "{}{}\r{:?}", clear::All, cursor::Goto(1,1), current_location).unwrap();
     //stdout().flush().unwrap();
      
 }
 
 
 pub fn main_menu(current_location: &mut (i16, i16)) {
-    let list: Vec<(String, bool, (i16, i16))> = vec![(String::from("Start"), false, (1,1)),    (String::from("Start"), false, (1,2)),  (String::from("Start"), false, (1,3)),];
+    let mut list: Vec<(String, bool, (i16, i16))> = vec![(String::from("Start"), false, (1,1)),    (String::from("Stop"), false, (1,2)),  (String::from("Restart"), false, (1,3)),];
+        write!(stdout(), "{}{}\r", clear::All, cursor::Goto(1,1)).unwrap();
+        stdout().flush().unwrap();
+
         for i in 0..list.len() {
-            let (_option, _active, _location): &(String, bool, (i16, i16)) =  &list[i];
+
+            match list[i].2 == *current_location {
+
+                true => {
+                    list[i].1 = true;
+                    break;
+                    },
+
+                _=> continue,
             
+                }
         }
+
+        for i in 0..list.len() {
+            if list[i].1 == true {
+                write!(stdout(), "{}{}{}\r\n", color::Fg(color::Red), list[i].0, color::Fg(color::Reset)).unwrap();
+                stdout().flush().unwrap();
+            } else {
+                write!(stdout(), "{}\r\n", list[i].0).unwrap();
+                stdout().flush().unwrap();
+            }
+        }
+            
+        
 }
